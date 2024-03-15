045e336ad49d672d84ab3f5d5c45616fa91abd25
UNIRy <163549399+UNIRy@users.noreply.github.com>
2024-03-15 18:06:46 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+header {
+  background-color: red;
+  padding: 15px;
+  border: 1px groove black;
+}
+
+footer {
+  border: 1px groove black;
+}
+
+.columns {
+  display: flex;
+}
+
+.column {
+  flex: 2%;
+  border: 1px groove black;
+}
